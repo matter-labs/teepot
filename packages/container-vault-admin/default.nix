9c01b0a2810bc84e9f9aa529d13383b1e16242f7
Harald Hoyer <harald@matterlabs.dev>
2024-06-12 13:22:03 +0200

    feat: add `container-vault-admin`
    Signed-off-by: Harald Hoyer <harald@matterlabs.dev>

--- packages/container-vault-admin/default.nix
+++ packages/container-vault-admin/default.nix
@@ -1,27 +1,29 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2024 Matter Labs
 { dockerTools
-, nixsgx
-, teepot
 , buildEnv
+, teepot
+, openssl
 , curl
+, nixsgx
 }:
 dockerTools.buildLayeredImage {
-  name = "vault-unseal";
+  name = "vault-admin";
 
-  config.Entrypoint = [ "${teepot.teepot.vault_unseal}/bin/vault-unseal" ];
+  config.Entrypoint = [ "${teepot.teepot.vault_admin}/bin/vault-admin" ];
 
   contents = buildEnv {
     name = "image-root";
     paths = with dockerTools; with nixsgx;[
-      azure-dcap-client
+      openssl.out
       curl.out
       sgx-dcap.quote_verify
+      sgx-dcap.default_qpl
       usrBinEnv
       binSh
       caCertificates
       fakeNss
-      teepot.teepot.vault_unseal
+      teepot.teepot.vault_admin
     ];
     pathsToLink = [ "/bin" "/lib" "/etc" ];
   };
