b066cdd15adb47dfa5a098dafe9d6c317898ec95
Harald Hoyer <harald@matterlabs.dev>
2024-12-20 09:01:50 +0100

    fix: update build process for teepot package
    - Fix output format for propagated-user-env-packages.
    - Remove empty bin directory after binaries are moved.

--- packages/teepot/default.nix
+++ packages/teepot/default.nix
@@ -35,10 +35,11 @@
       for i in $outputs; do
         [[ $i == "out" ]] && continue
         mkdir -p "''${!i}/bin"
-        echo "''${!i}" >> $out/nix-support/propagated-user-env-packages
+        echo -n "''${!i} " >> $out/nix-support/propagated-user-env-packages
         binname=''${i//_/-}
         mv "$out/bin/$binname" "''${!i}/bin/"
       done
+      rmdir "$out/bin"
     '';
   }
 )
